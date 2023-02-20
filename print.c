b8fb93a54dabc805c29cb06e0cfe63f302269991
ccryp <121784346+ccryp@users.noreply.github.com>
2023-02-20 09:15:49 +0900

    Add files via upload

---
+++ print.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print_test()
+{
+    printf("cryptolab under attacked!!!\n");
+}
---
+++ test.c
@@ -0,0 +1,7 @@
+#include "print.h"
+#include <stdio.h>
+
+int main()
+{
+    print_test();
+}
